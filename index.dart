844f61d272f3a91236624fc2970d82dcc0f73808
mahmoud <malmalah04@gmail.com>
2022-08-25 22:02:54 +0200

    xzxzxzx

---
+++ index.dart
@@ -0,0 +1,105 @@
+void main()
+{
+// int m = 1;
+// int d = 20;
+// print(d+m);
+//--------------------
+// double t =10.5;
+// double f =20.5;
+// print(t+f);
+//--------------------
+// String l = 'mahmoud';
+// String n = 'almalh';
+// print('$n $l');
+//--------------------
+// bool k = t==d;
+// print(k);
+//-------------------
+// List<String> a = ['1','2'];
+// print(a[1]);
+//-------------------
+// Map<String , String> r = {
+//   'name':'mahmoud',
+//   'numpur':'01026475912',
+//   'age':'19'
+// };
+// print('${r['age']}, ${r['name']}');
+//-------------------
+// var c;
+// c='md';
+// double z;
+// z=22;
+//-------------------
+// const int v = 9;
+// final int g;
+//-------------------
+// int tt = 5;
+// int aa = 6;
+// print(tt+aa);
+// print(tt-aa);
+// print(tt*aa);
+// print(tt/aa);
+// print(tt~/aa);
+// print(tt%aa);
+// print(++tt);
+// print(--aa);
+//---------------------
+//int nn = 2;
+//int aa = 1;
+//bool s = nn > aa;
+//bool s = nn < aa;
+//bool s = nn >= aa;
+//bool s = nn <= aa;
+//bool s = nn == aa;
+//bool s = nn != aa;
+//print(s);
+//---------------------
+// String cc = 'mahmoud';
+// bool qq = cc is bool;
+// print(qq);
+//---------------------
+// int xx = 7;
+// int ww = 3;
+// print(xx & ww);
+//--------------------
+// var vv = 4;
+// vv = 5;
+// vv ??= 5;
+// vv += 5;
+// vv *= 5;
+// vv /= 5;
+// print(vv);
+//-------------------
+// int x = 5;
+// double n = 1.5;
+// int m = 2;
+// if(x >= 1 && x <= 600)
+//   {
+//     var t = x * n;
+//     print('$t' + 'mahmoud');
+//   };
+// if(x >= 600 && x <= 1000)
+//   {
+//     var t = x * m;
+//     print(t);
+//   };
+//--------------------
+// int x = 2;
+// int y = 3;
+//
+// String s = x > y ? '$x is true' : '$y is fulse';
+// print(s);
+//----------------------
+// for(var i = 0; i<=20; i++){
+//   print(20);
+// }
+//-----------------------
+// print(addiy(5, 7));
+//------------------------
+
+
+}
+// int addiy(int x,int y)
+// {
+//   return x + y;
+// }
